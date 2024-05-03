93f81a46f6e3ffee4559e84f9957a0353dc0a384
manthanchauhan <manthanchauhan@indiagold.co>
2024-05-03 10:37:29 +0530

    cleanup

--- src/main/java/com/example/car_service_agency_new/timeslot/service/TimeslotService.java
+++ src/main/java/com/example/car_service_agency_new/timeslot/service/TimeslotService.java
@@ -1,6 +1,5 @@
 package com.example.car_service_agency_new.timeslot.service;
 
-import com.example.car_service_agency_new.timeslot.domain.Timeslot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +8,7 @@ public class TimeslotService {
     @Autowired
     private TimeslotRepository timeslotRepository;
 
-    public Timeslot getTimeslotById(Long timeslotId) {
-        return this.timeslotRepository.findById(timeslotId).orElseThrow(() -> new RuntimeException("Timeslot not found"));
+    public void getTimeslotById(Long timeslotId) {
+        this.timeslotRepository.findById(timeslotId).orElseThrow(() -> new RuntimeException("Timeslot not found"));
     }
 }
